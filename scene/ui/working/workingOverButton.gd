8adc7a5eeff56244953cfdb11be39ec517fcf888
凪白厨 <1463262325@qq.com>
2025-04-29 16:40:36 +0800

    4.28数据设计

--- scene/ui/working/workingOverButton.gd
+++ scene/ui/working/workingOverButton.gd
@@ -1,13 +1,22 @@
 extends Control
 
-var timeLeft=60
-var timeMax=60;
+var timeLeft=20
+var timeMax=20;
 
 func _ready():
 	rerend()
 func rerend():
-	$DismissButton/leftTime.value=100.0*timeLeft/timeMax;
+	var val=100.0*timeLeft/timeMax;
+	$DismissButton/timeLeft.value=val
+	if(val<=30):
+		$DismissButton/timeLeft.modulate.g=val/30;
+		$DismissButton/timeLeft.modulate.b=val/30;
+	else:
+		$DismissButton/timeLeft.modulate=Color.WHITE
+	
 
 func _process(delta):
 	timeLeft-=delta;
 	rerend()
+	if(timeLeft<=0):
+		SongGlobalSignal.emit_signal("WK_WorkOver");
--- scripts/common/SongGlobalSignal.gd
+++ scripts/common/SongGlobalSignal.gd
@@ -4,3 +4,5 @@ signal WK_SelectSong(id:String)
 signal WK_DeselectSong(id:String)
 
 signal WK_ChangeSelectedPerson(newPerson:String)
+
+signal WK_WorkOver()
